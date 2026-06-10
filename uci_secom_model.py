5a87836cf36511f5fc6973ae97d46d36533b9384
Lee Jong Min <intern-lee.jeong.min@codecomplete.jp>
2026-06-10 23:55:58 +0900

    uci_secom_model.py 업뎃

--- uci_secom_model.py
+++ uci_secom_model.py
@@ -16,4 +16,51 @@ plt.rcParams['font.family'] = 'Malgun Gothic'  # 윈도우 기본 한글 폰트
 plt.rcParams['axes.unicode_minus'] = False  # 마이너스 기호 깨짐 방지
 
 # 1. CSV 파일 불러오기
-df = pd.read_csv('uci-secom.csv')  # 경로는 실제 위치에 맞게 수정
+df = pd.read_csv('csv/uci-secom.csv')  # 경로는 실제 위치에 맞게 수정
+# print(df.info()) # 592개 컬럼. 
+print("==========================================")
+
+# 2. 피처와 라벨 분리 (Time 컬럼 제거)
+X = df.drop(['Pass/Fail', 'Time'], axis=1)  # Time 컬럼도 제거
+y = df['Pass/Fail']
+
+# 3. 결측치 처리 (평균 대체)
+imputer = SimpleImputer(strategy='mean')
+X_imputed = imputer.fit_transform(X)
+
+# 4. 학습/테스트 분리
+X_train, X_test, y_train, y_test = train_test_split(
+    X_imputed, y, test_size=0.2, stratify=y, random_state=42
+)
+
+# 5. 모델 학습
+model = RandomForestClassifier(n_estimators=100, random_state=42) # 랜덤포레스트 분류 모델 사용
+model.fit(X_train, y_train)
+
+# 6. 예측 및 평가
+y_pred = model.predict(X_test)
+print("🔍 Classification Report:")
+print(classification_report(y_test, y_pred))
+
+print("==========================================")
+#정확도
+print(f"정확도: {accuracy_score(y_test, y_pred)}")
+
+# 7. 혼동행렬 시각화
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+            xticklabels=['정상', '불량'], yticklabels=['정상', '불량'])
+plt.xlabel("예측값")
+plt.ylabel("실제값")
+plt.title("혼동 행렬")
+plt.show()
+
+# 8. 중요 센서 TOP 20 시각화
+importances = pd.Series(model.feature_importances_, index=X.columns)
+top_features = importances.sort_values(ascending=False).head(20)
+
+top_features.plot(kind='barh')
+plt.title("중요 센서 TOP 20")
+plt.gca().invert_yaxis()
+plt.show()
+
